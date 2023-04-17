6e0ebfadd5b47ed63b28bb7b5b6124e55d444e07
codeManeesha <maneeshasingh19jun@gmail.com>
2023-04-17 22:30:32 +0530

    success-story card created

--- gym_website_room_10/src/App.js
+++ gym_website_room_10/src/App.js
@@ -5,15 +5,18 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact';
 import Navbar from './Navbar/Navbar';
+import Card from './Pages/SuccessStory/Card'
 function App() {
   return (
    <>
    <Navbar/>
+ 
    <Routes>
     <Route path='/' Component={Home}/>
     <Route path='/about' Component={About}/>
     <Route path='/contact' Component={Contact}/>
    </Routes>
+   <Card/>
    </>
     
   );
